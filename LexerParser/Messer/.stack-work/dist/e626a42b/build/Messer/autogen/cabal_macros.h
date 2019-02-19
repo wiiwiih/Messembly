c060d61c1308d99f93ebc753319527e6ebceadf6
Vilma Lappi <wiiwiih@hotmail.com>
2019-02-19 16:16:44 +0200

    Jatkoa tulkille

--- LexerParser/Messer/.stack-work/dist/e626a42b/build/Messer/autogen/cabal_macros.h
+++ LexerParser/Messer/.stack-work/dist/e626a42b/build/Messer/autogen/cabal_macros.h
@@ -22,6 +22,17 @@
   (major1) == 4 && (major2) == 12 && (minor) <= 0)
 #endif /* MIN_VERSION_base */
 
+/* package containers-0.6.0.1 */
+#ifndef VERSION_containers
+#define VERSION_containers "0.6.0.1"
+#endif /* VERSION_containers */
+#ifndef MIN_VERSION_containers
+#define MIN_VERSION_containers(major1,major2,minor) (\
+  (major1) <  0 || \
+  (major1) == 0 && (major2) <  6 || \
+  (major1) == 0 && (major2) == 6 && (minor) <= 0)
+#endif /* MIN_VERSION_containers */
+
 /* package directory-1.3.3.0 */
 #ifndef VERSION_directory
 #define VERSION_directory "1.3.3.0"
@@ -177,7 +188,7 @@
 #endif /* MIN_TOOL_VERSION_strip */
 
 #ifndef CURRENT_COMPONENT_ID
-#define CURRENT_COMPONENT_ID "Messer-0.1.0.0-LT3aYZdj2Jz3M8ekYjp8hG-Messer"
+#define CURRENT_COMPONENT_ID "Messer-0.1.0.0-Gm7rgtTydiEHjGrkrFaboT-Messer"
 #endif /* CURRENT_COMPONENT_ID */
 #ifndef CURRENT_PACKAGE_VERSION
 #define CURRENT_PACKAGE_VERSION "0.1.0.0"
--- LexerParser/Messer/src/Interpreter.hs
+++ LexerParser/Messer/src/Interpreter.hs
@@ -3,6 +3,7 @@ module Interpreter where
 
 import Messer
 import Data.Maybe
+import qualified Data.Map.Strict as Map
 
 type Ohjelma = Luokka
 
@@ -15,34 +16,145 @@ iTest = do
 
         else print "rip"
 
-data IArvo = I Int | B Bool | S String deriving (Show)
+data IArvo = I Int | B Bool | S String | Void deriving (Show)
 
-type Muuttujat = [(String, IArvo)]
+
+type Muuttujat = Map.Map String IArvo
+
+type Aliohjelmat = Map.Map String (Palautustyyppi, [Parametri], [Lauseke])
 
 interpret :: a -> IO ()
 interpret = undefined
 
-eval :: Maaritelma -> Muuttujat -> IArvo
-eval (MId (Id x)) m = case lookup x m of
-                        Nothing -> error ("Muuttujaa " ++ x ++ " ei ole määritelty")
-                        Just a  -> a
-eval (MArvo x) m = case x of 
-                        ArvoInt a -> I a
-                        ArvoBool a -> B a
-                        ArvoString a -> S a
-eval (Aritmeettinen op x y) m = case op of
-                        Plus -> I ((iluku (eval x m)) + (iluku (eval y m)))
-                        Miinus -> I ((iluku (eval x m)) - (iluku (eval y m)))
-                        Kerto -> I ((iluku (eval x m)) * (iluku (eval y m)))
-                        Jako -> I (div (iluku (eval x m)) (iluku (eval y m)))
-eval (Vertailu vop x y) m = case vop of
-                        Pienempi          -> B ((iluku (eval x m)) < (iluku (eval y m)))
-                        Suurempi          -> B ((iluku (eval x m)) > (iluku (eval y m)))
-                        PienempiYhtasuuri -> B ((iluku (eval x m)) <= (iluku (eval y m)))
-                        SuurempiYhtasuuri -> B ((iluku (eval x m)) >= (iluku (eval y m)))
-                        Yhtasuuri         -> B ((iluku (eval x m)) == (iluku (eval y m)))
-                        Erisuuri          -> B ((iluku (eval x m)) /= (iluku (eval y m)))
+exec :: Lauseke -> Muuttujat -> Aliohjelmat -> IO Muuttujat
+exec (LTulostus x) m a = do
+                evalehto <- eval x m a
+                case evalehto of
+                    I i -> print i
+                    B b -> print b
+                    S s -> print s
+                    Interpreter.Void -> return ()
+                return m
+--tyyppejä ei tarkasteta vielä
+exec (LSijoitus x) m a = case x of
+                UusiSijoitus t (Id id) y -> do
+                                    arvo <- eval y m a
+                                    return (Map.insert id arvo m)
+                VanhaSijoitus (Id id) y -> do
+                                    arvo <- eval y m a
+                                    return (Map.insert id arvo m)
+exec (LPalautus x) m a = do
+                arvo <- eval x m a
+                return (Map.singleton "return" arvo)
+exec (LEhto lause) m a = case lause of
+                If ehto xs -> do
+                            -- lasketaan ehdon arvo ja katsotaan että se on bool
+                            evalehto <- eval ehto m a
+                            case evalehto of
+                                -- Jos ehto on totta suoritetaan lausekkeet ja 
+                                -- palautetaan niiden määrittämä ympäristö.
+                                -- Muuten ympärisöä ei muuteta
+                                B b -> do
+                                    if b 
+                                        then lausekkeet xs m a
+                                        else return m
+                                _ -> error ("Ei bool, rip")
+                IfElse ehto xs ys -> do
+                                -- Muuten sama kuin yllä, mutta jos ehto on epätosi,
+                                -- suoritetaan toiset lausekkeet 
+                                evalehto <- eval ehto m a
+                                case evalehto of
+                                    B b -> do
+                                        if b 
+                                            then lausekkeet xs m a
+                                            else lausekkeet ys m a
+                                    _ -> error ("Ei bool, rip")
+exec (LSilmukka ehto xs) m a = do
+                evalehto <- eval ehto m a
+                case evalehto of
+                    B b -> do
+                        if b 
+                            then do
+                                uusiM <- lausekkeet xs m a
+                                exec (LSilmukka ehto xs) uusiM a
+                            else 
+                                return m
+                    _ -> error ("Ei bool, rip")
 
+                                
+lausekkeet :: [Lauseke] -> Muuttujat -> Aliohjelmat -> IO Muuttujat
+lausekkeet [] m a = return m
+lausekkeet (x:xs) m a = do
+                uusiM <- exec x m a
+                lausekkeet xs uusiM a
+
+
+
+--tulkkaa määritelmiä
+eval :: Maaritelma -> Muuttujat -> Aliohjelmat -> IO IArvo
+eval (MId (Id x)) m a = case Map.lookup x m of
+                        Nothing -> error ("Muuttujaa " ++ x ++ " ei ole määritelty")
+                        Just y  -> return y
+eval (MArvo x) m a = case x of 
+                        ArvoInt y -> return (I y)
+                        ArvoBool y -> return (B y)
+                        ArvoString y -> return (S y)
+eval (Aritmeettinen op x y) m a  = case op of
+                        Plus -> do
+                            arvo1 <- eval x m a
+                            arvo2 <- eval y m a
+                            return (I ((iluku arvo1) + (iluku arvo2)))
+                        Miinus -> do
+                            arvo1 <- eval x m a
+                            arvo2 <- eval y m a
+                            return (I ((iluku arvo1) - (iluku arvo2)))
+                        Kerto -> do
+                            arvo1 <- eval x m a
+                            arvo2 <- eval y m a
+                            return (I ((iluku arvo1) * (iluku arvo2)))
+                        Jako -> do
+                            arvo1 <- eval x m a
+                            arvo2 <- eval y m a
+                            return (I (div (iluku arvo1) (iluku arvo2)))
+eval (Vertailu vop x y) m a = case vop of
+                        Pienempi          -> do
+                                arvo1 <- eval x m a
+                                arvo2 <- eval y m a
+                                return (B ((iluku arvo1) < (iluku arvo2)))
+                        Suurempi          -> do
+                                arvo1 <- eval x m a
+                                arvo2 <- eval y m a
+                                return (B ((iluku arvo1) > (iluku arvo2)))
+                        PienempiYhtasuuri -> do
+                                arvo1 <- eval x m a
+                                arvo2 <- eval y m a
+                                return (B ((iluku arvo1) <= (iluku arvo2)))
+                        SuurempiYhtasuuri -> do
+                                arvo1 <- eval x m a
+                                arvo2 <- eval y m a
+                                return (B ((iluku arvo1) >= (iluku arvo2)))
+                        Yhtasuuri         -> do
+                                arvo1 <- eval x m a
+                                arvo2 <- eval y m a
+                                return (B ((iluku arvo1) == (iluku arvo2)))
+                        Erisuuri          -> do
+                                arvo1 <- eval x m a
+                                arvo2 <- eval y m a
+                                return (B ((iluku arvo1) /= (iluku arvo2)))
+eval (MAKutsu (AKutsu (Id id) xs)) m a = case Map.lookup id a of
+                        Nothing -> error ("Aliohjelmaa ei löydy :(")
+                        Just (t, param, ys) -> undefined 
+                        ---jatka tästä
+                        --
+                        --
+                        --
+                                    
+params :: [Maaritelma] -> [Parametri] -> Muuttujat -> Aliohjelmat -> Muuttujat -> IO Muuttujat
+params [] [] am a um = return um
+params (x:xs) ((Parametri t (Id id)):ys) am a um = do
+                                arvo <- eval x am a
+                                return (Map.insert id arvo um)
+params _ _ _ _ _ = error ("Parametrien määrä ei täsmää")
 
 testt :: Maaritelma
 testt = Vertailu Pienempi (Aritmeettinen Kerto (MArvo (ArvoInt 2)) (MArvo (ArvoInt 3))) (MArvo (ArvoInt 1))
